621afe3cafafd81c850cb76521ec9c71baecb528
Victor Ventura <vmventura025@gmail.com>
2024-05-09 09:49:48 -0300

    Initial Commit

---
+++ src/main/java/br/com/fiap/sistemaEmpregados/SistemaEmpregadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sistemaEmpregados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEmpregadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEmpregadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/sistemaEmpregados/SistemaEmpregadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sistemaEmpregados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEmpregadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
